0253405d8f9dde0b0427af9fe68ed2fc9595a86e
w10 <ghostwarriortv@protonmail.com>
2020-04-03 12:01:11 +0200

    First_migration.

---
+++ Domain/Migrations/202004030953120_init.cs
@@ -0,0 +1,30 @@
+namespace Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        EmployeeID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        City = c.String(),
+                        Country = c.String(),
+                        Salary = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.EmployeeID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
