f48de1b6082f4540ddc7f6e5ea3c38fd6f28686f
w10 <ghostwarriortv@protonmail.com>
2020-04-10 12:09:44 +0200

    Database-Seed

---
+++ Domain/Migrations/202004100959366_Database-Seed.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DatabaseSeed : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Employees (FirstName, LastName, City, Country, Salary) VALUES ('Zbigniew','Nowak', 'Kraków', 'Polska', 2800)");
+            Sql("INSERT INTO Employees (FirstName, LastName, City, Country, Salary) VALUES ('Tomasz','Kowalski', 'Warszawa', 'Polska', 3500)");
+            Sql("INSERT INTO Employees (FirstName, LastName, City, Country, Salary) VALUES ('Józef','Głowa', 'Gdańsk', 'Polska', 4000)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
