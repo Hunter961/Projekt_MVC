de7ee9deb644a56b08ee07112a9e93ec883822ef
w10 <ghostwarriortv@protonmail.com>
2020-04-10 20:29:19 +0200

    database_update_test

---
+++ Domain/Migrations/202004101822129_update_table.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class update_table : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE Employees SET FirstName = 'Mieczysław' where EmployeeID = 3");
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
