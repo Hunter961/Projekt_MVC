750ebb127363f30ea306ce74cb750a6772af5ab0
w10 <ghostwarriortv@protonmail.com>
2020-04-27 13:47:01 +0200

    repo + db context creation

---
+++ Domain/Repos/EmployeeRepo.cs
@@ -0,0 +1,17 @@
+﻿using Projekt_MVC.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repos
+{
+
+    public class EmployeeRepo : IEmployeeRepo
+
+    {
+        private DomainDbContext _context = new DomainDbContext();
+
+    }
+}
