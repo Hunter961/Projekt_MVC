92c62db9e1e6d4590c87b1af012fd01633f145fc
w10 <ghostwarriortv@protonmail.com>
2020-05-12 13:21:28 +0200

    changes

---
+++ Projekt_MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt_MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return Content("W budowie");
+
+        }
+
+        public ActionResult Create()
+        {
+
+            return Content("W budowie");
+
+        }
+        
+
+        public ActionResult Details()
+        {
+
+            return Content("W budowie");
+
+        }
+
+
+        public ActionResult Edit()
+        {
+            return Content("W Budowie");
+
+        }
+
+
+        }
+
+
+    }
